113d2e85824891c3c3b08f5884f1836587cec8cc
Danilo Costa <80839381+DaniloCostat@users.noreply.github.com>
2023-03-02 17:11:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+   const elemento =  document.querySelector(seletorAudio).play();
+
+   if (elemento === null) {
+    alert('Elemento não encontrado');
+    console.log('elemento não encontrado');
+   }
+
+   if(elemento != null) {
+    elemento.play();
+   }
+   
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for( let contador = 0; contador < listaDeTeclas.length; contador ++){
 
-while(contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
     listaDeTeclas[contador].onclick = function(){
-        tocaSom('#som_tecla_pom');
+        tocaSom(idAudio);
     }
-    contador += 1;
-    
-    console.log(contador);
 
+    tecla.onkeydown =  function(evento){
+        console.log(evento.code === 'Space');
+        if(evento.code === 'Space' || evento.code === 'Enter' ){
+         tecla.classList.add('ativa');
+         
+        }
+        
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
